257e31da6683ff3d3055a19fb391dd44e9ac4dba
minebinroyal1@gmail.com <minebinroyal1@gmail.com>
2025-01-12 22:22:05 +0700

    3.19-3.21

--- models/person.js
+++ models/person.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const url = process.env.MONGODB_URI;
 
 mongoose
-  .connect(url)
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("connected to mongodb");
   })
@@ -13,38 +14,34 @@ mongoose
     console.log("error connecting to mongodb:", error.message);
   });
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-});
-
-personSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+const personSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minLength: 3,
+      required: true,
+    },
+    number: {
+      type: String,
+      validate: {
+        validator: function (v) {
+          return /^\d{2,3}-\d+$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid phone number!`,
+      },
+      minLength: 8,
+      required: true,
+    },
   },
-});
+  {
+    toJSON: {
+      transform: (doc, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+      },
+    },
+  }
+);
 
 export default mongoose.model("Person", personSchema);
-
-// if (process.argv.length > 3) {
-//   const name = process.argv[3];
-//   const number = process.argv[4];
-//   const person = new Person({
-//     name: name,
-//     number: number,
-//   });
-
-//   person.save().then((result) => {
-//     console.log(`added ${name} number ${number} to phonebook`);
-//     mongoose.connection.close();
-//   });
-// } else {
-//   Person.find({}).then((result) => {
-//     result.forEach((person) => {
-//       console.log(person.name, person.number);
-//     });
-//     mongoose.connection.close();
-//   });
-// }
